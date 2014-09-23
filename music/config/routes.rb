116073b8893e305103ed8042f2a0ae34423bf51f
ncaskey04 <ncaskey04@gmail.com>
2014-09-23 10:54:57 -0700

    user login routes kinda work

--- music/config/routes.rb
+++ music/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   get 'articles/index'
+
+  get '/login', :to => 'sessions#new', :as => :login
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
 
   root 'sites#index'
-  # resources :sites
+
+  # resources :users
+  resources :sites
   # match "*path", to: "sites#index", via: "get"
   # root 'sites#index'
 
