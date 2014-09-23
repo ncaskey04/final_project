4dd17c8423cd8823728fbcb24ac4cd5ca18d61b5
ncaskey04 <ncaskey04@gmail.com>
2014-09-23 10:22:20 -0700

    added sites controller with index view

--- music/config/routes.rb
+++ music/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
   get 'articles/index'
-  root 'articles#index'
-
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
 
+  root 'sites#index'
   # resources :sites
   # match "*path", to: "sites#index", via: "get"
   # root 'sites#index'
