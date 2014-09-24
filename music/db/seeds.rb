156a3e12c0dbdc494a44f51b15abeb4102704094
ncaskey04 <ncaskey04@gmail.com>
2014-09-24 12:00:10 -0700

    test

--- music/db/seeds.rb
+++ music/db/seeds.rb
@@ -5,10 +5,10 @@
 # homer = User.create({email: "homersimpson@ga.com", first_name: "Homer", last_name: "Simpson", password: "password",image_url: "http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png"})
 # ned = User.create({email: "nedflanders@ga.com", first_name: "Ned", last_name: "Flanders", password: "password",image_url: "http://upload.wikimedia.org/wikipedia/en/8/84/Ned_Flanders.png"})
 
-nick = User.create({
-  name: "Nick Caskey",
-  provider: "spotify",
-  uid: "1233510636",
-  token: "BQA8yFV3j2Hq9H4haG2abrQ3qTh9gTgBGtPEV9d5irVcYTr2CckKB0ixmIf9ErQ95WlcWT8XtOb-UID1wCSWSU_uUFlTYegEpWzgG5jgSzOYZiuw5kdJlHpMwPL1MY_5XGxuuc6CRf5edqy2eZI_fVhXoRJuhRQtZxbxQYAtjQg",
-  refresh_token: "AQCvXQaKTsVMTgqOIl46Lj3fOzxSwpK23twMr9rgTcrtrvzyImJG69wlGmvNERQmdDutcJD4CrzPp3mYMGGAEmv-B735Qd6e0zYXnpLwzm_19ExD1UPa7FFlfHH4IUhPCjk"
-})
+  nick = User.create({
+    name: "Nick Caskey",
+    provider: "spotify",
+    uid: "1233510636",
+    token: "BQA8yFV3j2Hq9H4haG2abrQ3qTh9gTgBGtPEV9d5irVcYTr2CckKB0ixmIf9ErQ95WlcWT8XtOb-UID1wCSWSU_uUFlTYegEpWzgG5jgSzOYZiuw5kdJlHpMwPL1MY_5XGxuuc6CRf5edqy2eZI_fVhXoRJuhRQtZxbxQYAtjQg",
+    refresh_token: "AQCvXQaKTsVMTgqOIl46Lj3fOzxSwpK23twMr9rgTcrtrvzyImJG69wlGmvNERQmdDutcJD4CrzPp3mYMGGAEmv-B735Qd6e0zYXnpLwzm_19ExD1UPa7FFlfHH4IUhPCjk"
+  })
