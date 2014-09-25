b55f239fe2e3fc50f590cc7527de8f42a94b2818
ncaskey04 <ncaskey04@gmail.com>
2014-09-24 18:04:34 -0700

    we have the data we need must plot points to map

--- music/app/assets/javascripts/index.js
+++ music/app/assets/javascripts/index.js
@@ -20,8 +20,31 @@ function getResult(search){
     dataType: "json",
   }).done(function(data) {
     console.log(data);
+    for (i = 0; i < data.albums.items.length; i ++){
+      var album = data.albums.items[i];
+      for (j=0; j < album.available_markets.length; j++){
+        var market = album.available_markets[j];
+      }  
+    }
+    // looop thru albums
+        // loop thru album country codes
+             // getLatLng for country code
+             // put on map
   });
 }
+
+var getLatLong = function(market){
+  for (var i = 0; i < countries.length; i++){
+    var iso2Code = countries[i].iso2Code;
+    var latitude = countries[i].latitude;
+    var longitude = countries[i].longitude;
+    if (iso2Code === market){
+      return {lat: latitude, lon: longitude};
+    } else {
+      console.log("This is not a valid market");
+    }
+  }
+};
 
 // function searchAlbum(id){
 //  $.ajax({
